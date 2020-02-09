998e839f3001a819a0977fbcc44f3cca45ecb896
e-razdumina <e.razdumina@gmail.com>
2020-02-09 18:14:53 +0300

    ДЗ к 9 уроку

---
+++ Topic_9.sql
@@ -0,0 +1,197 @@
+-- Task 1.1
+-- В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. 
+-- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. 
+-- Используйте транзакции.
+
+DROP DATABASE IF EXISTS shop;
+
+DROP DATABASE IF EXISTS sample;
+
+CREATE DATABASE shop; 
+
+USE shop; 
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+ 
+CREATE DATABASE sample; 
+
+USE sample; 
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+START TRANSACTION; 
+
+
+USE sample;
+
+INSERT INTO
+  sample.users
+SELECT
+  *
+FROM
+  shop.users 
+WHERE 
+	users.id = 1
+;
+
+SELECT * FROM users;
+
+COMMIT;
+
+
+-- Task 1.2
+-- Создайте представление, которое выводит название name товарной позиции из таблицы products 
+-- и соответствующее название каталога name из таблицы catalogs.
+
+USE shop; 
+
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+ 
+ DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+ 
+CREATE OR REPLACE VIEW prod AS
+SELECT products.name AS product, catalogs.name AS catalog
+FROM products
+JOIN catalogs 
+ON products.catalog_id = catalogs.id
+;
+
+SELECT * FROM prod;
+
+-- Task 2.1
+-- Создайте двух пользователей которые имеют доступ к базе данных shop. Первому пользователю shop_read должны быть 
+-- доступны только запросы на чтение данных, второму пользователю shop — любые операции в пределах базы данных shop.
+
+-- GRANT SELECT ON shop.* TO 'shop_read'@'localhost' ;
+
+-- GRANT ALL ON shop.* TO 'shop'@'localhost' ;
+-- RANT GRANT ON shop.* TO 'shop'@'localhost' ;
+
+
+-- Task 3.1
+-- Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. 
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу 
+-- "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP FUNCTION IF EXISTS hello;
+
+DELIMITER //
+CREATE FUNCTION  hello()
+RETURNS TEXT NO SQL
+BEGIN
+	DECLARE hour INT;
+  	SET hour = HOUR(NOW());
+
+	IF(hour > 6 AND hour < 12) THEN
+	RETURN "Доброе утро";
+  	ELSEIF (hour > 12 AND hour < 18) THEN
+	RETURN "Добрый день";
+	ELSEIF (hour > 18) THEN
+	RETURN "Добрый вечер";
+  	ELSE
+	RETURN "Доброй ночи";
+  END IF;
+ 
+END
+//
+DELIMITER ;
+
+SELECT hello ();
+
+
+-- Task 3.2
+-- В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
+-- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение 
+-- NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. 
+-- При попытке присвоить полям NULL-значение необходимо отменить операцию.
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+DELIMITER //
+
+CREATE TRIGGER check_name_and_desc_products BEFORE UPDATE ON products
+FOR EACH ROW BEGIN
+  IF products.name IS NULL AND products.description IS NULL THEN
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'DELETE canceled';
+  END IF;
+END//
+
+DELIMITER ;
+
