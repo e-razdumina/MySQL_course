5696a45efc7f6f8b7f5247baef5055678dd20df3
e-razdumina <e.razdumina@gmail.com>
2020-02-17 10:21:26 +0300

    дз к 11 уроку

---
+++ Topic_11.sql
@@ -0,0 +1,117 @@
+-- Task 1.1
+-- Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, 
+-- catalogs и products в таблицу logs помещается время и дата создания записи, название таблицы, 
+-- идентификатор первичного ключа и содержимое поля name.
+
+DROP DATABASE IF EXISTS shop;
+CREATE DATABASE shop;
+USE shop;
+
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs ( 
+created_at DATETIME, 
+table_name VARCHAR(255) COMMENT 'Имя таблицы', 
+key_id INT COMMENT 'Идентификатор ключа', 
+name VARCHAR(255) COMMENT 'Содержимое поля name'
+) COMMENT = 'Лог' 
+ENGINE=Archive;
+
+DROP TRIGGER IF EXISTS log_products;
+
+DELIMITER //
+CREATE TRIGGER insert_log_products AFTER INSERT ON products
+FOR EACH ROW BEGIN
+  INSERT INTO logs values (NOW(), "products", NEW.id, NEW.name);
+END//
+
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS log_catalogs;
+
+DELIMITER //
+CREATE TRIGGER insert_log_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW BEGIN
+  INSERT INTO logs values (NOW(), "catalogs", NEW.id, NEW.name);
+END//
+
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS log_users;
+
+DELIMITER //
+CREATE TRIGGER insert_log_users AFTER INSERT ON users
+FOR EACH ROW BEGIN
+  INSERT INTO logs values (NOW(), "users", NEW.id, NEW.name);
+END//
+
+DELIMITER ;
+
+
+
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+SELECT * FROM logs;
+
+-- Task 2.1
+-- В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
+
+
+
+-- Task 2.2
+-- При помощи базы данных Redis решите задачу поиска имени пользователя по электронному адресу и наоборот, поиск электронного адреса пользователя по его имени.
+
+-- Task 2.3
+-- Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
+
